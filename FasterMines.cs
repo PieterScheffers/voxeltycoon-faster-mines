c2c7d5f1114e7098bcd4731e95b2b126228c6867
Pieter Scheffers <pieter.scheffers@gmail.com>
2021-04-28 21:22:04 +0200

    First version of mod. Make mines 30 times as fast

---
+++ FasterMines.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using VoxelTycoon.Buildings;
+
+public class KamikazeFishFasterMinesMod : VoxelTycoon.Modding.Mod
+{
+    protected override void Initialize()
+    {
+        var harmony = new Harmony("kamikaze_fish.faster_mines");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(MineSharedData), "OutputInterval", MethodType.Getter)]
+class MineSharedData_getOutputInterval_FasterMinesPatch
+{
+    static void Postfix(ref float __result)
+    {
+        __result = __result / 30;
+    }
+}
